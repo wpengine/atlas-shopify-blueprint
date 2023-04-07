2c465d090b21d7b6e444fc31ca36f7f7876c4cdf
Alvaro Cavalcanti <alvaro.cavalcanti@wpengine.com>
2023-04-07 18:28:47 +0100

    refactor: :recycle: Extracted CtaButton component into its own module

--- components/PromoSection/PromoSection.js
+++ components/PromoSection/PromoSection.js
@@ -1,16 +1,6 @@
 import { Container } from '../Container';
+import { CtaButton } from '../CtaButton';
 import styles from './PromoSection.module.scss';
-import Link from 'next/link';
-
-const Cta = ({ ctaLink = null, ctaLabel = null }) => {
-  return (ctaLink && ctaLabel) ? (
-    <div className='buttonContainer'>
-      <Link href={ctaLink}>
-        <a className='button'>{`${ctaLabel}`}</a>
-      </Link>
-    </div>
-  ) : null;
-};
 
 const PromoSection = ({ showCta = false }) => {
   return (
@@ -18,7 +8,7 @@ const PromoSection = ({ showCta = false }) => {
       <Container>
         <h1 className={styles.title}>Promo Banners</h1>
         <div className={styles.description}>You can use this component to promote articles or specific products. And optionally add a CTA below.</div>
-        { showCta && <Cta ctaLink='/about' ctaLabel='About' /> }
+        {showCta && <CtaButton ctaLink='/about' ctaLabel='About' />}
       </Container>
     </div>
   );
