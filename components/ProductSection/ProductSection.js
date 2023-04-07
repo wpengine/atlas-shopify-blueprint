ca678fcbce2dd579d7a31026e46f580a64d67858
Alvaro Cavalcanti <alvaro.cavalcanti@wpengine.com>
2023-04-07 18:18:59 +0100

    style: :recycle: Using the section style from the global scss

--- components/ProductSection/ProductSection.js
+++ components/ProductSection/ProductSection.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import classNames from 'classnames';
 import styles from './ProductSection.module.scss';
 import { ProductCard } from '../ProductCard';
 import { Container } from '../Container';
 
-const cx = classNames.bind(styles);
-
 const ProductSection = ({ products, heading }) => {
   return (
-    <div className={cx(['section', styles.component])}>
+    <div className={`section ${styles.component}`}>
       <Container>
         <h2>{heading}</h2>
-        <div className={cx(styles.section)}>
+        <div className='section'>
           {products?.map?.((product) => {
             return <ProductCard key={product.id} product={product} />;
           })}
--- components/TestimonialsSection/TestimonialsSection.js
+++ components/TestimonialsSection/TestimonialsSection.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import TestimonialCard from './TestimonialCard';
 import { Container } from '../Container';
-import classNames from 'classnames';
 import styles from './TestimonialsSection.module.scss';
 
-const cx = classNames.bind(styles);
 
 const TestimonialsSection = () => {
   return (
-    <div className={cx(['section', styles.component])}>
+    <div className={`section ${styles.component}`}>
       <Container>
         <h2>Customer Testimonials</h2>
-        <div className={cx(styles.section)}>
+        <div className='section'>
           <TestimonialCard
             image='https://demo.studiopress.com/page-builder/spend/eso_spend_customer_testimonial_1.jpg'
             blurb='I can sync all my BigCommerce products into WordPress and catch all updates automatically. Now I can create content in WordPress with BigCommerce products!'
