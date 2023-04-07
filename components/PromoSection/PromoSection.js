12866f9e89d97238c9b9ba76eb8308a42ef22bda
Alvaro Cavalcanti <alvaro.cavalcanti@wpengine.com>
2023-04-07 14:02:29 +0100

    feat: :sparkles: Added CTA to the PromoSection component
    Also removed unused React import and refactored the CSS

--- components/PromoSection/PromoSection.js
+++ components/PromoSection/PromoSection.js
@@ -1,16 +1,27 @@
-import React from 'react';
 import { Container } from '../Container';
 import classNames from 'classnames';
 import styles from './PromoSection.module.scss';
+import Link from 'next/link';
 
 const cx = classNames.bind(styles);
 
-const PromoSection = () => {
+const Cta = ({ ctaLink = null, ctaLabel = null }) => {
+  return (ctaLink && ctaLabel) ? (
+    <div className={styles.buttonContainer}>
+      <Link href={ctaLink}>
+        <a className={styles.button}>{`${ctaLabel}`}</a>
+      </Link>
+    </div>
+  ) : null;
+};
+
+const PromoSection = ({ ctaLink = null, ctaLabel = null }) => {
   return (
-    <div className={cx(['section', styles.component])}>
+    <div className={`section ${styles.component}`}>
       <Container>
         <h1 className={cx(styles.title)}>Promo Banners</h1>
-        <div className={cx(styles.description)}>You can use this component to promote articles or specific products.</div>
+        <div className={cx(styles.description)}>You can use this component to promote articles or specific products. And optionally add a CTA below.</div>
+        <Cta ctaLink={ctaLink} ctaLabel={ctaLabel} />
       </Container>
     </div>
   );
