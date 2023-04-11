2b6149f796c61b444e17f3ce6b8ad757ab75826b
Alvaro Cavalcanti <alvaro.cavalcanti@wpengine.com>
2023-04-11 17:07:19 +0100

    refactor: :recycle: Lifted CtaButton state to PromoSection

--- components/PromoSection/PromoSection.js
+++ components/PromoSection/PromoSection.js
@@ -2,13 +2,19 @@ import { Container } from '../Container';
 import { CtaButton } from '../CtaButton';
 import styles from './PromoSection.module.scss';
 
-const PromoSection = ({ showCta = false }) => {
+const PromoSection = ({
+  showCta = false,
+  ctaLink = null,
+  ctaLabel = null,
+  title = null,
+  description = null
+}) => {
   return (
     <div className={`section ${styles.component}`}>
       <Container>
-        <h1 className={styles.title}>Promo Banners</h1>
-        <div className={styles.description}>You can use this component to promote articles or specific products. And optionally add a CTA below.</div>
-        <CtaButton showCta={showCta} ctaLink='/about' ctaLabel='About' />
+        <h1 className={styles.title}>{title}</h1>
+        <div className={styles.description}>{description}</div>
+        { showCta && <CtaButton ctaLink={ctaLink} ctaLabel={ctaLabel} /> }
       </Container>
     </div>
   );
