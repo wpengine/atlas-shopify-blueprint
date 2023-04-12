c5801e27b8abf677169c70abad351483e7302bdd
Alvaro Cavalcanti <alvaro.cavalcanti@wpengine.com>
2023-04-12 20:38:18 +0100

    test: :white_check_mark: Added testing infraestructure
    Also added a test for the PromoSection component

---
+++ components/__tests__/PromoSection.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import {PromoSection} from '../PromoSection';
+
+describe('<PromoSection />', () => {
+  it('displays the CTA button', () => {
+    render(<PromoSection showCta ctaLabel='Foo' ctaLink='/foo' />);
+
+    expect(screen.getByText('Foo')).toBeInTheDocument;
+  });
+
+  it('does not display the CTA button', () => {
+    render(<PromoSection showCta={false} ctaLabel='Foo' ctaLink='/foo' />);
+
+    expect(screen.findByText('Foo')).toBeNull;
+  });
+});
