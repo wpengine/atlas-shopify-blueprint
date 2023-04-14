6e26e06c0d8a150874ec953d5bc7601ee183b0c1
Michal Pawlinski <michal.pawlinski@wpengine.com>
2023-04-14 16:00:12 +0200

    [titan-306] cart stubs

---
+++ data/stubs/cart/multiple.ts
@@ -0,0 +1,101 @@
+const multiple = {
+  cart: {
+    id: "gid://shopify/Cart/c1-6bcda1657c8fa22e7188b08d5b217a2a",
+    lines: {
+      nodes: [
+        {
+          cost: {
+            amountPerQuantity: {
+              amount: "35.0",
+              currencyCode: "USD",
+            },
+            totalAmount: {
+              currencyCode: "USD",
+              amount: "35.0",
+            },
+            subtotalAmount: {
+              amount: "35.0",
+              currencyCode: "USD",
+            },
+          },
+          id: "gid://shopify/CartLine/2963f250-6e18-4934-b86e-0d7976af1826?cart=c1-6bcda1657c8fa22e7188b08d5b217a2a",
+          quantity: 1,
+          merchandise: {
+            id: "gid://shopify/ProductVariant/44876466749743",
+            title: "Blue",
+            product: {
+              id: "gid://shopify/Product/8235118264623",
+              featuredImage: {
+                altText: null,
+                url: "https://cdn.shopify.com/s/files/1/0695/2478/3407/products/hoodie-purple.jpg?v=1681383898",
+              },
+              title: "Triangulum Hoodie",
+            },
+          },
+        },
+        {
+          cost: {
+            amountPerQuantity: {
+              amount: "20.0",
+              currencyCode: "USD",
+            },
+            totalAmount: {
+              currencyCode: "USD",
+              amount: "40.0",
+            },
+            subtotalAmount: {
+              amount: "40.0",
+              currencyCode: "USD",
+            },
+          },
+          id: "gid://shopify/CartLine/d8f5be0b-d095-49e9-8433-344ef85fc149?cart=c1-6bcda1657c8fa22e7188b08d5b217a2a",
+          quantity: 2,
+          merchandise: {
+            id: "gid://shopify/ProductVariant/44876574720303",
+            title: "Default Title",
+            product: {
+              id: "gid://shopify/Product/8235141300527",
+              featuredImage: {
+                altText: null,
+                url: "https://cdn.shopify.com/s/files/1/0695/2478/3407/products/hat-2.jpg?v=1681385139",
+              },
+              title: "Toasty Cap Cotton",
+            },
+          },
+        },
+        {
+          cost: {
+            amountPerQuantity: {
+              amount: "35.0",
+              currencyCode: "USD",
+            },
+            totalAmount: {
+              currencyCode: "USD",
+              amount: "35.0",
+            },
+            subtotalAmount: {
+              amount: "35.0",
+              currencyCode: "USD",
+            },
+          },
+          id: "gid://shopify/CartLine/ed2ec47a-7c9d-4c96-88ff-f00f452d5f1d?cart=c1-6bcda1657c8fa22e7188b08d5b217a2a",
+          quantity: 1,
+          merchandise: {
+            id: "gid://shopify/ProductVariant/44876558500143",
+            title: "Red",
+            product: {
+              id: "gid://shopify/Product/8235135271215",
+              featuredImage: {
+                altText: null,
+                url: "https://cdn.shopify.com/s/files/1/0695/2478/3407/products/hoodie-gray_b0bf23fa-aac6-451a-a1b0-23bc81114a1e.jpg?v=1681384795",
+              },
+              title: "Andromeda Hoodie",
+            },
+          },
+        },
+      ],
+    },
+  },
+};
+
+export default multiple;
