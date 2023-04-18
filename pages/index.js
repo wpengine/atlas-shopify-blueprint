eab6058adb06bc0e274ce7ea746476d6e528d711
Alvaro Cavalcanti <alvaro.cavalcanti@wpengine.com>
2023-04-18 13:52:03 +0100

    TITAN-307, TITAN-322, TITAN-308 - Using Shopify API Client on the Front Page, Shop Page, and Individual Product Page (#28)
    * build: :pushpin: Updated dependencies
    
    * feat: Added Shopify Client and first GQL fragment
    
    * feat: First iteration of the ProductsContext
    
    * build: :pushpin: Pinning Node version to match Atlas environment
    
    * build: :heavy_plus_sign: Adding a dependency on the same Node version as the Node engine
    
    Also switched to a different SCSS transform lib
    
    * build: :pushpin:
    
    * feat: Added types modeling the products API response
    
    * feat: :sparkles: Converted API client to TypeScript, added Context to front-page
    
    TBD Need to fix compilation issue
    
    * deps: Updated dependencies
    
    * feat: :sparkles: Replaced product stubs with data fetched from Shopify
    
    TBD: Replace useState with SSG
    
    * feat: :construction: WIP Trying to pass the products as static props
    
    * fix: fetch products from getStaticProps in wp-templates
    
    * refactor: remove api dir
    
    * fix: remove console log
    
    * feat: individual product on product page
    
    * refactor: :fire: Removed unecessary context file. Deleted console log statement
    
    * feat: read creds from env vars
    
    * feat: all products on shop page
    
    * refactor: use ssr on shop page
    
    ---------
    
    Co-authored-by: Ross Maguire <ross.rmdesigns@gmail.com>
    Co-authored-by: Ross Maguire <48026075+RossoMaguire@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import { getWordPressProps, WordPressTemplate } from '@faustwp/core';
+import shopifyClient from '../utilities/shopifyClient';
+import { GET_PRODUCTS } from '../queries/Products';
 
 export default function Page(props) {
   return <WordPressTemplate {...props} />;
 }
 
-export function getStaticProps(ctx) {
-  return getWordPressProps({ ctx });
+export async function getStaticProps(ctx) {
+  const staticProps = await getWordPressProps({ ctx, revalidate: 5 });
+  const { data } = await shopifyClient.query({ query: GET_PRODUCTS });
+  const { products } = data;
+
+  if (staticProps.props && products.nodes.length) {
+    staticProps.props.products = products.nodes;
+  }
+
+  return staticProps;
 }
