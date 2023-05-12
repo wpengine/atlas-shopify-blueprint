03e230108491ccce04ba06615fb0f30d8cbe493a
Ross Maguire <ross.rmdesigns@gmail.com>
2023-05-12 14:26:50 +0100

    fix: add variant id to stubs

--- data/stubs/cart/single.ts
+++ data/stubs/cart/single.ts
@@ -29,13 +29,13 @@ const single = {
           merchandise: {
             id: 'gid://shopify/ProductVariant/44876466749743',
             title: 'Blue',
+            image: {
+              altText: null,
+              url: 'https://cdn.shopify.com/s/files/1/0695/2478/3407/products/hoodie-purple.jpg?v=1681383898',
+            },
             product: {
               id: 'gid://shopify/Product/8235118264623',
               handle: 'triangulum-hoodie',
-              featuredImage: {
-                altText: null,
-                url: 'https://cdn.shopify.com/s/files/1/0695/2478/3407/products/hoodie-purple.jpg?v=1681383898',
-              },
               title: 'Triangulum Hoodie',
             },
           },
--- data/stubs/products.js
+++ data/stubs/products.js
@@ -40,6 +40,7 @@ const productsStub = {
             nodes: [
               {
                 sku: 'RWV-1',
+                id: 'gid://shopify/ProductVariant/44876432343343',
                 image: {
                   url: 'https://cdn.shopify.com/s/files/1/0695/2478/3407/products/shirt-charcoal.jpg?v=1681383187',
                 },
@@ -96,6 +97,7 @@ const productsStub = {
             nodes: [
               {
                 sku: 'QRK-1',
+                id: 'gid://shopify/ProductVariant/44876447842607',
                 image: {
                   url: 'https://cdn.shopify.com/s/files/1/0695/2478/3407/products/shirt-blue_efbc40ad-505f-4cc7-9be7-4ba8a6fa3e85.jpg?v=1681383505',
                 },
@@ -152,6 +154,7 @@ const productsStub = {
             nodes: [
               {
                 sku: 'TPG-1',
+                id: 'gid://shopify/ProductVariant/44876453544239',
                 image: {
                   url: 'https://cdn.shopify.com/s/files/1/0695/2478/3407/products/shirt-gray_2717712f-efe9-443b-a417-dc074b129a14.jpg?v=1681383619',
                 },
@@ -208,6 +211,7 @@ const productsStub = {
             nodes: [
               {
                 sku: 'TRI-1',
+                id: 'gid://shopify/ProductVariant/44876466749743',
                 image: {
                   url: 'https://cdn.shopify.com/s/files/1/0695/2478/3407/products/hoodie-blue.jpg?v=1681383898',
                 },
@@ -225,6 +229,7 @@ const productsStub = {
               },
               {
                 sku: 'TRI-2',
+                id: 'gid://shopify/ProductVariant/44876466782511',
                 image: {
                   url: 'https://cdn.shopify.com/s/files/1/0695/2478/3407/products/hoodie-purple.jpg?v=1681383898',
                 },
@@ -242,6 +247,7 @@ const productsStub = {
               },
               {
                 sku: 'TRI-3',
+                id: 'gid://shopify/ProductVariant/44876466815279',
                 image: {
                   url: 'https://cdn.shopify.com/s/files/1/0695/2478/3407/products/hoodie-red.jpg?v=1681383898',
                 },
@@ -259,6 +265,7 @@ const productsStub = {
               },
               {
                 sku: 'TRI-4',
+                id: 'gid://shopify/ProductVariant/44876466848047',
                 image: {
                   url: 'https://cdn.shopify.com/s/files/1/0695/2478/3407/products/hoodie-gray.jpg?v=1681383898',
                 },
@@ -315,6 +322,7 @@ const productsStub = {
             nodes: [
               {
                 sku: 'PIN-1',
+                id: 'gid://shopify/ProductVariant/44876533039407',
                 image: {
                   url: 'https://cdn.shopify.com/s/files/1/0695/2478/3407/products/hoodie-blue_17265ea5-f14f-43d6-9225-e4a44b417340.jpg?v=1681384577',
                 },
@@ -332,6 +340,7 @@ const productsStub = {
               },
               {
                 sku: 'PIN-2',
+                id: 'gid://shopify/ProductVariant/44876533072175',
                 image: {
                   url: 'https://cdn.shopify.com/s/files/1/0695/2478/3407/products/hoodie-purple_36aece3f-1b8b-454c-8995-f0098cb6f912.jpg?v=1681384577',
                 },
@@ -349,6 +358,7 @@ const productsStub = {
               },
               {
                 sku: 'PIN-3',
+                id: 'gid://shopify/ProductVariant/44876533104943',
                 image: {
                   url: 'https://cdn.shopify.com/s/files/1/0695/2478/3407/products/hoodie-charcoal.jpg?v=1681384577',
                 },
@@ -366,6 +376,7 @@ const productsStub = {
               },
               {
                 sku: 'PIN-4',
+                id: 'gid://shopify/ProductVariant/44876533137711',
                 image: {
                   url: 'https://cdn.shopify.com/s/files/1/0695/2478/3407/products/hoodie-red_5e0b7f2e-c002-4c73-9de5-23161b3a358d.jpg?v=1681384577',
                 },
@@ -422,6 +433,7 @@ const productsStub = {
             nodes: [
               {
                 sku: 'ADR-1',
+                id: 'gid://shopify/ProductVariant/44876558467375',
                 image: {
                   url: 'https://cdn.shopify.com/s/files/1/0695/2478/3407/products/hoodie-gray_b0bf23fa-aac6-451a-a1b0-23bc81114a1e.jpg?v=1681384795',
                 },
@@ -439,6 +451,7 @@ const productsStub = {
               },
               {
                 sku: 'ADR-2',
+                id: 'gid://shopify/ProductVariant/44876558500143',
                 image: {
                   url: 'https://cdn.shopify.com/s/files/1/0695/2478/3407/products/hoodie-red_2d71bcca-cb82-4450-838b-73717467f29f.jpg?v=1681384795',
                 },
@@ -456,6 +469,7 @@ const productsStub = {
               },
               {
                 sku: 'ADR-3',
+                id: 'gid://shopify/ProductVariant/44876558532911',
                 image: {
                   url: 'https://cdn.shopify.com/s/files/1/0695/2478/3407/products/hoodie-purple_fc7ec2d8-2aa2-46f3-8cd6-26e4fa30114d.jpg?v=1681384795',
                 },
@@ -473,6 +487,7 @@ const productsStub = {
               },
               {
                 sku: 'ADR-4',
+                id: 'gid://shopify/ProductVariant/44876558565679',
                 image: {
                   url: 'https://cdn.shopify.com/s/files/1/0695/2478/3407/products/hoodie-charcoal_82062466-c51f-4d87-b678-22b9998f58fb.jpg?v=1681384795',
                 },
@@ -529,6 +544,7 @@ const productsStub = {
             nodes: [
               {
                 sku: 'TCW-1',
+                id: 'gid://shopify/ProductVariant/44876565774639',
                 image: {
                   url: 'https://cdn.shopify.com/s/files/1/0695/2478/3407/products/hat-3.jpg?v=1681384941',
                 },
@@ -579,6 +595,7 @@ const productsStub = {
             nodes: [
               {
                 sku: 'TCC-1',
+                id: 'gid://shopify/ProductVariant/44876574720303',
                 image: {
                   url: 'https://cdn.shopify.com/s/files/1/0695/2478/3407/products/hat-2.jpg?v=1681385139',
                 },
@@ -629,6 +646,7 @@ const productsStub = {
             nodes: [
               {
                 sku: 'TCE-1',
+                id: 'gid://shopify/ProductVariant/44876576915759',
                 image: {
                   url: 'https://cdn.shopify.com/s/files/1/0695/2478/3407/products/hat-1.jpg?v=1681385217',
                 },
