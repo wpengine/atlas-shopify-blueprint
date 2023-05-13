8b2ba4d4be2a62b699066113e054c06b410e94ca
Kacper Leczynski <kacper.leczynski@wpengine.com>
2023-05-13 15:00:04 +0100

    test: :white_check_mark: Refacor FormatDate test and one more test case added to ProductSection
    Refacor FormatDate test and one more test case added to ProductSection so to check out other options for generating items

--- components/FormatDate/__tests__/FormatDate.test.js
+++ components/FormatDate/__tests__/FormatDate.test.js
@@ -1,6 +1,5 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-// import { PostInfo } from "../../PostInfo";
 import FormatDate from "../FormatDate";
 
 describe("<FormatDate />", () => {
--- components/ProductSection/__tests__/ProductSection.test.js
+++ components/ProductSection/__tests__/ProductSection.test.js
@@ -25,4 +25,74 @@ describe("<ProductSection />", () => {
     expect(screen.getByTestId("img")).toBeVisible();
     expect(screen.getByText("Latest Products")).toBeVisible();
   });
+
+  it("Render 4 products and check if 4 appear", () => {
+    const products = [
+      {
+        id: 1,
+        title: "Radiowave Shirt",
+        variants: {
+          nodes: [
+            {
+              price: {
+                amount: "18.0",
+              },
+            },
+          ],
+        },
+      },
+      {
+        id: 2,
+        title: "Quark Shirt",
+        variants: {
+          nodes: [
+            {
+              price: {
+                amount: "20.0",
+              },
+            },
+          ],
+        },
+      },
+      {
+        id: 3,
+        title: "Topography Shirt",
+        variants: {
+          nodes: [
+            {
+              price: {
+                amount: "20.0",
+              },
+            },
+          ],
+        },
+      },
+      {
+        id: 4,
+        title: "Triangulum Hoodie",
+        variants: {
+          nodes: [
+            {
+              price: {
+                amount: "35.0",
+              },
+            },
+          ],
+        },
+      },
+    ];
+    const heading = "Latest Products";
+
+    render(
+      <ProductSection
+        key={[products.id]}
+        heading={heading}
+        products={products}
+      />
+    );
+
+    const renderedProducts = screen.getAllByRole("listitem");
+
+    expect(renderedProducts).toHaveLength(4);
+  });
 });
