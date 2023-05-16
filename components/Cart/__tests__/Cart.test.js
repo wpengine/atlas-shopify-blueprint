a4f7619348d6425952c2d003019bad6acf105c19
Ross Maguire <ross.rmdesigns@gmail.com>
2023-05-16 11:39:23 +0100

    test: fail to remove from cart

--- components/Cart/__tests__/Cart.test.js
+++ components/Cart/__tests__/Cart.test.js
@@ -4,6 +4,7 @@ import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { ShopifyCartProvider } from '../../../hooks/useShopifyCart';
 import CREATE_CART from '../../../mutations/CreateCart';
 import RETRIEVE_CART from '../../../queries/Cart';
+import REMOVE_FROM_CART from '../../../mutations/RemoveFromCart';
 import empty from '../../../data/stubs/cart/empty';
 import multiple from '../../../data/stubs//cart/multiple';
 import { CART_COOKIE } from '../../../constants/carts';
@@ -100,6 +101,62 @@ describe('<Cart />', () => {
         expect(
           screen.getByText(
             /Triangulum Hoodie has been removed from your cart./i
+          )
+        ).toBeVisible();
+      });
+    });
+  });
+
+  it('remove item from cart unsuccessfully and presents an error', async () => {
+    const retrieveCartMock = {
+      request: {
+        query: RETRIEVE_CART,
+        variables: {
+          id: 'gid://shopify/Cart/c1-c63c275d6f27eb309d4efac08dee2e7d',
+        },
+      },
+      result: { data: multiple },
+    };
+
+    const removeFromCartMock = {
+      request: {
+        query: REMOVE_FROM_CART,
+        variables: {
+          cartId: 'gid://shopify/Cart/c1-c63c275d6f27eb309d4efac08dee2e7d',
+          lines: [
+            {
+              quantity: 1,
+              merchandiseId: 'gid://shopify/ProductVariant/44876432343343',
+            },
+          ],
+        },
+      },
+      error: new Error('An error occurred'),
+    };
+
+    Object.defineProperty(window.document, 'cookie', {
+      writable: true,
+      value: `${CART_COOKIE}=gid://shopify/Cart/c1-c63c275d6f27eb309d4efac08dee2e7d`,
+    });
+
+    render(
+      <MockedProvider mocks={[retrieveCartMock]} addTypename={true}>
+        <ShopifyCartProvider>
+          <Cart />
+        </ShopifyCartProvider>
+      </MockedProvider>
+    );
+
+    waitFor(() => {
+      expect(screen.getByText(/Triangulum Hoodie/i)).toBeVisible();
+
+      const remove = screen.getByText(/Triangulum Hoodie/i).closest('svg');
+      fireEvent.click(remove);
+
+      waitFor(() => {
+        expect(
+          screen.getByText(
+            /There was an issue removing this item from the cart./i
           )
         ).toBeVisible();
       });
