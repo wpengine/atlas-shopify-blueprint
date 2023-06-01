369c37218bb271511ddbad50e262fe4d21158349
Ross Maguire <ross.rmdesigns@gmail.com>
2023-06-01 10:25:05 +0100

    style: add padding to message

--- utilities/ConnectionUnavailable.js
+++ utilities/ConnectionUnavailable.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const ConnectionUnavailable = () => {
-    return <p>To get started connecting your Shopify store, please make sure your storefront API token and URL is added as Atlas environment variables.</p>;
-}
+  return (
+    <p style={{ padding: '20px' }}>
+      To get started connecting your Shopify store, please make sure your
+      storefront API token and URL is added as Atlas environment variables.
+    </p>
+  );
+};
 
 export default ConnectionUnavailable;
