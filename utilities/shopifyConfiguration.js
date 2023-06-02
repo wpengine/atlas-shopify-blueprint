a8d4748def959e4acc6fdc8ffc08ace7add0282d
Noah-Kelleher <127727372+Noah-Kelleher@users.noreply.github.com>
2023-06-02 10:51:35 +0100

    fix: Update utilities/shopifyConfiguration.js
    Co-authored-by: Alvaro Cavalcanti <alvaro.cavalcanti@wpengine.com>

--- utilities/shopifyConfiguration.js
+++ utilities/shopifyConfiguration.js
@@ -1,10 +1,6 @@
 const shopifyConfiguration = {
     available: function() {
-        if (!process.env.NEXT_PUBLIC_SHOPIFY_URL || !process.env.NEXT_PUBLIC_SHOPIFY_TOKEN) {
-            return false;
-        } else {
-            return true;
-        }
+        return process.env.NEXT_PUBLIC_SHOPIFY_URL && process.env.NEXT_PUBLIC_SHOPIFY_TOKEN;
     }
 };
 
