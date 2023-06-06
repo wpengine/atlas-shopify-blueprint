ab8d8227c274e14201c60bc7c6e68b3e1439fbb1
Alvaro Cavalcanti <alvaro.cavalcanti@wpengine.com>
2023-06-06 12:51:57 +0100

    refactor: :recycle: Removed unnecessary variable and simplified return statement based on falsy value

--- utilities/shopifyConfiguration.js
+++ utilities/shopifyConfiguration.js
@@ -1,15 +1,12 @@
 const shopifyConfiguration = {
   available: function () {
-    let urlIsValid = true;
-    const tokenExists = process.env.NEXT_PUBLIC_SHOPIFY_TOKEN !== '';
-
     try {
       new URL(process.env.NEXT_PUBLIC_SHOPIFY_URL);
     } catch (err) {
       return false;
     }
 
-    return urlIsValid && tokenExists;
+    return process.env.NEXT_PUBLIC_SHOPIFY_TOKEN && true;
   },
 };
 
