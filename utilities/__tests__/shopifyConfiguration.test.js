31ecb9ad3f8fe5f2f3cfd260705b1b0a8949c9c9
Alvaro Cavalcanti <alvaro.cavalcanti@wpengine.com>
2023-06-06 12:09:57 +0100

    style: :art: Formatted the code

--- utilities/__tests__/shopifyConfiguration.test.js
+++ utilities/__tests__/shopifyConfiguration.test.js
@@ -1,75 +1,75 @@
 import shopifyConfiguration from "../shopifyConfiguration";
 
 describe('shopifyConfiguration', () => {
-    describe('When the environment variables are set', () => {
-      it('returns true', () => {
-          expect(shopifyConfiguration.available()).toBeTruthy();
-      });
+  describe('When the environment variables are set', () => {
+    it('returns true', () => {
+      expect(shopifyConfiguration.available()).toBeTruthy();
     });
-    describe('When the environment variables does not exist', () => {
-      const OLD_ENV = process.env;
+  });
+  describe('When the environment variables does not exist', () => {
+    const OLD_ENV = process.env;
 
-      beforeEach(() => {
-        jest.resetModules();
-        delete process.env.NEXT_PUBLIC_SHOPIFY_TOKEN;
-        delete process.env.NEXT_PUBLIC_SHOPIFY_URL;
-      });
-
-      afterAll(() => {
-        process.env = { ...OLD_ENV };
-      });
-
-      it('returns false', () => {
-          expect(shopifyConfiguration.available()).toBe(false);
-      });
-    });
-    describe('When the environment variables are set to null', () => {
-      const OLD_ENV = process.env;
-
-      beforeEach(() => {
-        jest.resetModules();
-        process.env.NEXT_PUBLIC_SHOPIFY_TOKEN = null;
-        process.env.NEXT_PUBLIC_SHOPIFY_URL = null;
-      });
-
-      afterAll(() => {
-        process.env = { ...OLD_ENV };
-      });
-
-      it('returns false', () => {
-          expect(shopifyConfiguration.available()).toBe(false);
-      });
-    });
-    describe('When the NEXT_PUBLIC_SHOPIFY_URL is invalid', () => {
-      const OLD_ENV = process.env;
-
-      beforeEach(() => {
-        jest.resetModules();
-        process.env.NEXT_PUBLIC_SHOPIFY_URL = "foo";
+    beforeEach(() => {
+      jest.resetModules();
+      delete process.env.NEXT_PUBLIC_SHOPIFY_TOKEN;
+      delete process.env.NEXT_PUBLIC_SHOPIFY_URL;
     });
 
-      afterAll(() => {
-        process.env = { ...OLD_ENV };
-      });
-
-      it('returns false', () => {
-          expect(shopifyConfiguration.available()).toBe(false);
-      });
-    });
-    describe('When the NEXT_PUBLIC_SHOPIFY_TOKEN is empty', () => {
-      const OLD_ENV = process.env;
-
-      beforeEach(() => {
-        jest.resetModules();
-        process.env.NEXT_PUBLIC_SHOPIFY_TOKEN = "";
+    afterAll(() => {
+      process.env = { ...OLD_ENV };
     });
 
-      afterAll(() => {
-        process.env = { ...OLD_ENV };
-      });
-
-      it('returns false', () => {
-          expect(shopifyConfiguration.available()).toBe(false);
-      });
+    it('returns false', () => {
+      expect(shopifyConfiguration.available()).toBe(false);
     });
+  });
+  describe('When the environment variables are set to null', () => {
+    const OLD_ENV = process.env;
+
+    beforeEach(() => {
+      jest.resetModules();
+      process.env.NEXT_PUBLIC_SHOPIFY_TOKEN = null;
+      process.env.NEXT_PUBLIC_SHOPIFY_URL = null;
+    });
+
+    afterAll(() => {
+      process.env = { ...OLD_ENV };
+    });
+
+    it('returns false', () => {
+      expect(shopifyConfiguration.available()).toBe(false);
+    });
+  });
+  describe('When the NEXT_PUBLIC_SHOPIFY_URL is invalid', () => {
+    const OLD_ENV = process.env;
+
+    beforeEach(() => {
+      jest.resetModules();
+      process.env.NEXT_PUBLIC_SHOPIFY_URL = "foo";
+    });
+
+    afterAll(() => {
+      process.env = { ...OLD_ENV };
+    });
+
+    it('returns false', () => {
+      expect(shopifyConfiguration.available()).toBe(false);
+    });
+  });
+  describe('When the NEXT_PUBLIC_SHOPIFY_TOKEN is empty', () => {
+    const OLD_ENV = process.env;
+
+    beforeEach(() => {
+      jest.resetModules();
+      process.env.NEXT_PUBLIC_SHOPIFY_TOKEN = "";
+    });
+
+    afterAll(() => {
+      process.env = { ...OLD_ENV };
+    });
+
+    it('returns false', () => {
+      expect(shopifyConfiguration.available()).toBe(false);
+    });
+  });
 });
