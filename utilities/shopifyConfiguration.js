52516accba1ca52a7f2a5ebc57f74d5bda137272
Noah-Kelleher <noah.kelleher@wpengine.com>
2023-06-06 14:30:31 +0100

    refactor: :art: Refactor to return token if URL is valid and return false and output error if invalid

--- utilities/shopifyConfiguration.js
+++ utilities/shopifyConfiguration.js
@@ -4,6 +4,7 @@ const shopifyConfiguration = {
         new URL(process.env.NEXT_PUBLIC_SHOPIFY_URL);
         return process.env.NEXT_PUBLIC_SHOPIFY_TOKEN;
       } catch (err) {
+        console.error(err);
         return false;
       }
     },
