03084bd9b8f6c53a2a1f6b0fe9d3d30a5d292280
Alvaro Cavalcanti <alvaro.cavalcanti@wpengine.com>
2023-06-12 12:30:06 +0100

    refactor: :recycle: Moved component definition outside of render

--- components/Heading/Heading.js
+++ components/Heading/Heading.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-export default function Heading({ level = 'h1', children, className }) {
-  const Tag = ({ ...props }) => React.createElement(level, props, children);
+const Tag = ({ level, children, ...props }) => React.createElement(level, props, children);
 
-  return <Tag className={className}>{children}</Tag>;
+export default function Heading({ level = 'h1', children, className }) {
+  return <Tag level={level} className={className}>{children}</Tag>;
 }
