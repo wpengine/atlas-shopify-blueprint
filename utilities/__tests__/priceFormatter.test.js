d0ad0b2910d390c72d174948b0e7164ebed0a90a
Ross Maguire <ross.rmdesigns@gmail.com>
2023-06-20 18:28:43 +0100

    test: price format other than USD

--- utilities/__tests__/priceFormatter.test.js
+++ utilities/__tests__/priceFormatter.test.js
@@ -6,24 +6,34 @@ describe('priceFormatter', () => {
 
     expect(price).toBe('$3.00');
   });
+
   it('formats a single decimal price to double digit', () => {
     const price = priceFormatter('3.5');
 
     expect(price).toBe('$3.50');
   });
+
   it('formats a double decimal price to double digit', () => {
     const price = priceFormatter('3.56');
 
     expect(price).toBe('$3.56');
   });
+
   it('formats an empty price to double digit zero', () => {
     const price = priceFormatter('');
 
     expect(price).toBe('$0.00');
   });
+
   it('formats a missing price to double digit zero', () => {
     const price = priceFormatter();
 
     expect(price).toBe('$0.00');
+  });
+
+  it('formats a price with a currency code other than USD', () => {
+    const price = priceFormatter('3.56', 'EUR');
+
+    expect(price).toBe('€3.56');
   });
 });
