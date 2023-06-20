31e0521eace03c80080a2c783b8aae7bd3046e3c
Ross Maguire <ross.rmdesigns@gmail.com>
2023-06-20 16:36:26 +0100

    fix: compareAtPrice was being rendered as current price when on sale

--- components/ProductDetails/ProductPrice.js
+++ components/ProductDetails/ProductPrice.js
@@ -4,24 +4,22 @@ import priceFormatter from '../../utilities/priceFormatter';
  * Render the ProductPrice component.
  *
  * @param {Props} props The props object.
- * @param {string | number} props.salePrice The sale price for this product, if available.
- * @param {string | number} props.price The price for this product.
+ * @param {string | number} props.compareAtPrice The original price for this product, if on sale.
+ * @param {string | number} props.price The current price for this product.
  * @param {string | number} props.currencyCode The currency code for this product's price.
  *
  * @returns {React.ReactElement} The ProductPrice component.
  */
 
-const ProductPrice = ({ salePrice, price, currencyCode }) => {
+const ProductPrice = ({ compareAtPrice, price, currencyCode }) => {
   return (
     <p className="price">
-      {salePrice !== null ? (
+      {compareAtPrice && (
         <>
-          <del>{priceFormatter(price, currencyCode)}</del>{' '}
-          {priceFormatter(salePrice?.amount, salePrice?.currencyCode)}
+          <del>{priceFormatter(compareAtPrice, currencyCode)}</del>{' '}
         </>
-      ) : (
-        <>{priceFormatter(price, currencyCode)}</>
       )}
+      {priceFormatter(price, currencyCode)}
     </p>
   );
 };
